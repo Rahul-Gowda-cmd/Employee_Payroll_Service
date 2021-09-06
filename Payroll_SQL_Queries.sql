e805e8abb318bbe5c260d4257662b9fcef3b4d28
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-09-06 12:23:56 +0530

    feat:Make Terissa as Part of Sales and Marketing Department

--- Payroll_SQL_Queries.sql
+++ Payroll_SQL_Queries.sql
@@ -50,3 +50,9 @@ deduction real ,
 taxable_pay money,
 income_tax money,
 net_pay float;
+
+insert into employee_payroll values
+('Terisa',700000.0,'2020-08-14','F','8734586245','sales','india',100.0,12000.0,2000.0,1233.0)
+
+update employee_payroll set taxable_pay=12000.00, department='marketing',income_tax=2000.00, net_pay=1233 where id=2;
+select * from employee_payroll where name='Terisa'
