f450f01d6e5fb4a8d4e429fb0bbd8b23cab7a282
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-09-06 12:29:23 +0530

    feat:Craeting new tables and making relations between tables

--- Payroll_SQL_Queries.sql
+++ Payroll_SQL_Queries.sql
@@ -56,3 +56,45 @@ insert into employee_payroll values
 
 update employee_payroll set taxable_pay=12000.00, department='marketing',income_tax=2000.00, net_pay=1233 where id=2;
 select * from employee_payroll where name='Terisa'
+
+create table Employee
+(
+emp_id int identity primary key,
+name varchar(50),
+startdate date,
+gender char(1),
+phone varchar(13),
+address varchar(50),
+);
+select * from Employee
+create table Department
+(
+dept_id int identity primary key,
+rooms int,
+emp_id int FOREIGN KEY REFERENCES employee(emp_id)
+);
+select * from Department
+
+create table Payroll
+(
+emp_id int FOREIGN KEY REFERENCES employee(emp_id),
+basicpay money,
+deduction money,
+taxable_pay money,
+income_tax money,
+net_pay money,
+);
+
+select * from Payroll
+
+create table Company
+(
+emp_id int foreign key references employee(emp_id),
+company_name varchar(50),
+);
+
+create table Employee_Department
+(
+emp_id int foreign key references employee(emp_id),
+dept_id int foreign key references Department(dept_id),
+);
